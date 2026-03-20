cdcae1c0c994bbbf39ec36b1de0401f09d56014f
LMJ-01 <audwns6302@naver.com>
2026-03-20 17:44:41 +0900

    feat:20260320 oauth2 back-front finished

--- oauth2_frontend/src/pages/homePage.tsx
+++ oauth2_frontend/src/pages/homePage.tsx
@@ -16,8 +16,36 @@ export default function HomePage() {
     }
 
     return (
-        <>
-        
-        </>
+        <Container maxWidth='sm'>
+            <Box sx={{mt: 8}}>
+                <Paper elevation={3} sx={{ p: 4, textAlign: 'center' }}>
+                    <Avatar sx={{ width: 80, height: 80, mx: 'auto', mb: 2, bgcolor: 'primary.main '}}>
+                        <PersonIcon fontSize="large" />
+                    </Avatar>
+
+                    {/* user?.name : user가 UserInfo 자료형을 따르는 객체거나 null이라는 뜻인데, null이면 name이 없습니다. 그때 오류 발생 안하고 undefined를 리턴해주게됩니다. */}
+                    <Typography variant="h5" fontWeight= 'bold' gutterBottom>
+                        환영합니다, {user?.name} 님 ! 😁
+                    </Typography>
+
+                    <Box sx={{ textAlign: 'left' , bgcolor: 'grey.50', p: 2, borderRadius: 1, mb: 3}}>
+                        <Typography variant="body2" color='text.secondary'>이메일</Typography>
+                        <Typography variant='body1' fontWeight='medium'>{user?.email}</Typography>
+                        <Typography variant="body2" color='text.secondary'>권한</Typography>
+                        <Typography variant='body1' fontWeight='medium'>{user?.role}</Typography>
+                    </Box>
+
+                    <Button
+                        variant='outlined'
+                        color='error'
+                        startIcon={<LogoutIcon />}
+                        onClick={handleLogout}
+                        fullWidth
+                    >
+                        logout
+                    </Button>
+                </Paper>
+            </Box>
+        </Container>
     );
 }
